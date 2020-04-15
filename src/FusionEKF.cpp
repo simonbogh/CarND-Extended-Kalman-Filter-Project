5cd75c42dafc314c768338dc0161897ff6f182bf
Simon Bøgh <sb@m-tech.aau.dk>
2020-04-15 23:15:35 +0200

    ADD: Call update function of the Kalman Filter

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -180,10 +180,18 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // TODO: Radar updates
-
+    // Calculate
+    Hj_ = tools.CalculateJacobian(ekf_.x_);
+    ekf_.H_ = Hj_;
+    ekf_.R_ = R_radar_;
+    // Update using Extende Kalman Filter
+    ekf_.UpdateEKF(measurement_pack.raw_measurements_);
   } else {
     // TODO: Laser updates
-
+    ekf_.H_ = H_laser_;
+    ekf_.R_ = R_laser_;
+    // Update using standard Kalman Filter
+    ekf_.Update(measurement_pack.raw_measurements_);
   }
 
   // print the output
